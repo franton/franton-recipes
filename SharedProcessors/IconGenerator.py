71bf0445f45886a86ac7b0f03216b7747c9e0b00
Richard Purves <franton@users.noreply.github.com>
2026-09-18 20:56:01 -0700

    Update IconGenerator.py

--- SharedProcessors/IconGenerator.py
+++ SharedProcessors/IconGenerator.py
@@ -24,7 +24,7 @@ import shutil
 import subprocess
 
 from autopkglib import Processor, ProcessorError
-from autopkglib import DmgMounter
+from autopkglib.DmgMounter import DmgMounter
 
 __all__ = ["IconGenerator"]
 
